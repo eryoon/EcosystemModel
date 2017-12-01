1c62359e929d69f710c88025f8fbae8a7f4918fc
Eric <eryoon2019@mirmanmustangs.org>
2017-12-01 10:50:45 -0800

    Init commit with files

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//NEED:
+//Population
+//Name
+//Predation rate
+//Chance of catching each animal
+//Food value (how much meat)
+//Food value needed to survive
+
+var species = [
+    {
+        "name": "Rabbit",
+        "population": 10,
+        "food-needed": 3,
+        "food-value": 1,
+        "diet": [
+            "Grass"
+        ],
+        "max-predation-count": 3
+    }
+];
